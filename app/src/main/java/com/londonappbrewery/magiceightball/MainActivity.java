4c2a147cb8902eb06c4142f7e7ef0c4ad2add35b
Umair Tahir <Umairtahir01@gmail.com>
2018-07-25 15:02:47 +0500

    Project Finish!

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView anss = (TextView)findViewById(R.id.ans);
+        final Button btn = (Button)findViewById(R.id.btn);
+        final String Answer_Message[] = {"No!","Yes!","I D'not know" };
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int number = random.nextInt(3);
+                anss.setText(Answer_Message[number]);
+            }
+        });
+
+
     }
 }
